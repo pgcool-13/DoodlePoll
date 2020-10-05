35e2ec57e3d51d80f28e5cb5a23a080416aa9bdd
PriyaGupta <priya.gupta13it@gmail.com>
2020-10-05 11:16:54 +0200

    Basic SpringBoot project structure created with required dependencies

---
+++ doodlepoll/src/main/java/com/doodlepoll/DoodlepollApplication.java
@@ -0,0 +1,13 @@
+package com.doodlepoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodlepollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodlepollApplication.class, args);
+	}
+
+}
---
+++ doodlepoll/src/test/java/com/doodlepoll/DoodlepollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doodlepoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodlepollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
